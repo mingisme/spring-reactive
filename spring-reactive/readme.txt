c526249b0e8b0d97f00849db38de173256c63535
ming.wang4 <ming.wang4@envision-digital.com>
2024-02-22 17:54:24 +0800

    webflux and mongodb reactive

---
+++ spring-reactive/readme.txt
@@ -0,0 +1,5 @@
+https://reflectoring.io/getting-started-with-spring-webflux/
+
+https://www.infoworld.com/article/3702629/reactive-java-with-spring-webflux-and-reactor.html
+
+
---
+++ spring-reactive/src/main/java/com/example/springreactive/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.springreactive;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends ReactiveMongoRepository<User, String> {
+}
